b3cb769655b557bc05b942e947e83cc6772f5f34
Lee SeungA <dltmddk981119@gmail.com>
2024-09-15 04:24:35 +0900

    20240915 다크모드 비활성화

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // 다크 모드를 비활성화
   theme: {
     extend: {
       colors: {
         primary: '#5477AD', // #5477AD 색상을 'primary'로 정의
         secondary: '#CEDBF0', // #CEDBF0 색상을 'secondary'로 정의
-        ggray : '#E0E0E0',
+        ggray: '#E0E0E0',
       },
     },
   },
   plugins: [],
-}
-
+};
