b8516203f6fa0eff7322a5cdda5f91a4068eb925
Lee SeungA <dltmddk981119@gmail.com>
2024-09-16 04:59:41 +0900

    20240916 Introduction 수정, 전체글씨 크기 수정

--- src/pages/Introduction.vue
+++ src/pages/Introduction.vue
@@ -2,36 +2,43 @@
   <!-- Main Content -->
   <section id="introduction" class="section-group">
     <div class="small-group">
-      <h1 class="big-title">Introduction</h1>
-      <p class="text-gray-700">내 소개 텍스트입니다. 여기에 자기소개를 작성하세요.</p>
+      <h1 class="big-title">👩🏻‍💻 Introduction</h1>
+      <p class="pt-2 text-xl font-semibold"> "다시 만나고 싶은 개발자가 되기"</p>
+      <p class="line-content"> 
+        <br> 안녕하세요. "다시 만나고 싶은 개발자"가 되고 싶은 이승아 입니다.☺️
+        <br> 프로젝트를 진행하면서 개발 실력이 좋은 것도 중요하지만, 의사소통 능력이 정말 중요하다는 것을 깨달았습니다.
+        <br> 가구 회사에서 2년간 판매 매니저로 근무하면서 많은 고객들을 만난 경험이 있습니다.
+        <br> 이 경험을 토대로 고객의 니즈를 충족시키는 것은 물론, 같이 일하는 동료에게도 함께 일하고 싶은 팀원이 되고 싶습니다.
+      </p>
     </div>
 
     <div id="education" class="small-group">
-      <h2 class="small-title">Education</h2>
-      <div class="">
+      <h2 class="small-title">🏫 Education</h2>
+      <div>
         <div class="line-content">
-          <div class="">
+          <div class="line-content-group-1">
             <p class="education-period">2014. 03 ~ 2017. 02</p>
             <p class="line-content-title">창동고등학교</p>
           </div>
-          <p>졸업</p>
+          <p class="line-content-group-2">졸업</p>
         </div>
+
         <div class="line-content">
-          <div>
+          <div class="line-content-group-1">
             <p class="education-period">2017. 03 ~ 2019. 02</p>
             <p class="line-content-title">세한대학교</p>
           </div>
-          <div class="line-content-right">
+          <div class="line-content-group-2">
             <p>항공서비스학과 수료</p>
             <p>3.87 / 4.5</p>
           </div>
         </div>
         <div class="line-content">
-          <div>
+          <div class="line-content-group-1">
             <p class="education-period">2019. 03 ~ 2021. 08</p>
             <p class="line-content-title">대진대학교 (편입)</p>
           </div>
-          <div class="line-content-right">
+          <div class="line-content-group-2">
             <p>영어영문학과 졸업</p>
             <p>4.01 / 4.5</p>
           </div>
@@ -40,33 +47,33 @@
     </div>
 
     <div id="activities" class="small-group">
-      <h2 class="small-title">Activities</h2>
+      <h2 class="small-title">🏃🏻‍♀️‍➡️ Activities</h2>
       <div class="line-content">
-        <div>
+        <div class="line-content-group-1">
           <p class="education-period">2024. 05. 07 ~ 2024. 10. 24</p>
           <p class="line-content-title">KB IT's Your Life (5기)</p>
         </div>
 
-        <p>수료</p>
-        <li class="line-content-right">KB국민은행에서 주관하는 부트캠프 수강</li>
+        <p class="line-content-group-2">수료</p>
+        <li class="line-content-group-3">KB국민은행에서 주관하는 부트캠프 수강</li>
       </div>
     </div>
 
     <div id="certifications" class="small-group">
-      <h2 class="small-title">Certifications</h2>
+      <h2 class="small-title">🪪 Certifications</h2>
       <div class="line-content">
-        <p class="line-content-title">정보처리기사 (필기 합격)</p>
-        <p>한국산업인력공단</p>
-        <div class="line-content-right">
+        <p class="line-content-title line-content-group-1">정보처리기사 (필기 합격)</p>
+        <p class="line-content-group-2">한국산업인력공단</p>
+        <div class="line-content-group-3">
           <p>2024. 09. 10</p>
-          <p>자격번호 :</p>
+          <p>자격번호 : -</p>
         </div>
       </div>
 
       <div class="line-content">
-        <p class="line-content-title">컴퓨터활용능력 (1급)</p>
-        <p>대한상공회의소</p>
-        <div class="line-content-right">
+        <p class="line-content-title line-content-group-1">컴퓨터활용능력 (1급)</p>
+        <p class="line-content-group-2">대한상공회의소</p>
+        <div class="line-content-group-3">
           <p>2021. 05. 07</p>
           <p>자격번호 : 21-K9-057497</p>
         </div>
@@ -74,10 +81,20 @@
     </div>
 
     <div id="awards" class="small-group">
-      <h2 class="small-title">Awards</h2>
+      <h2 class="small-title">🏆 Awards</h2>
       <div class="line-content">
-        <p class="line-content-title">정보처리기사 (필기 합격)</p>
-        <p>한국산업인력공단</p>
+        <div class="line-content-group-1">
+          <p class="education-period">2024. 10. 16</p>
+          <p class="line-content-title">KB IT's Your Life</p>
+          <p class="line-content-title">최종 프로젝트 최우수상</p>
+        </div>
+
+        <p class="line-content-group-2">국민은행<br>(멀티캠퍼스)</p>
+        <p class="line-content-group-3"><span class="font-semibold">✨30개 팀 중 1위✨</span>
+          <br>
+          사용자 소비 성향에 맞춘 자산 관리 서비스 
+        </p>
+
       </div>
     </div>
   </section>
