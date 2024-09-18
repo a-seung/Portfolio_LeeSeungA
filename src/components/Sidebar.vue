0b8bb5ab5adee4fc32e9df6d3d57f45446c42bc4
Lee SeungA <dltmddk981119@gmail.com>
2024-09-19 05:04:47 +0900

    20240919 main.css에 home-button 추가

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,12 @@
 <template>
-  <aside class="fixed w-60 h-screen bg-secondary py-10">
+  <aside class="fixed w-60 h-screen bg-secondary py-8">
     <div class="flex flex-col items-center">
-      <div @click.prevent="goHome" class="cursor-pointer text-lg font-semibold">이승아의 포트폴리오</div>
+      <div @click.prevent="goHome" class="home-button">이승아의 포트폴리오</div>
       <div class="rounded-full my-6 overflow-hidden  w-44 h-56">
         <img src="@/assets/images/profileImg.jpg" alt="Profile Image" class="w-full h-full object-cover rounded-[50%] aspect-[3/4]" />
       </div>
       <!-- 프로필 이미지 자리 -->
-      <nav class="w-full my-10">
+      <nav class="w-full mt-6">
         <div class="text-center">
           <div :class="['menu-item', activeSection === 'introduction' ? 'menu-item-active' : '']">
             <button @click.prevent="navigateToSection('introduction')">Introduction</button>
--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -15,27 +15,22 @@
         </div>
         <p class="line-content-title"></p>
       </div>
-
-      <div class="line-content">
-        <p class="line-content-title">Collaboration Tool</p>
-      </div>
-
     </div>
     <div class="h-80"></div>
   </section>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
 
-const router = useRouter()
+const router = useRouter();
 
 // 버튼 클릭 시 /project1 경로로 이동하는 함수
 const goToProject1 = () => {
-  router.push({ name: 'Project1' })
-}
+  router.push({ name: 'Project1' });
+};
 
 const goToProject2 = () => {
-  router.push({ name: 'Project2' })
-}
+  router.push({ name: 'Project2' });
+};
 </script>
